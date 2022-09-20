d805ae150a82c0d1f71bb87adea2269bd1702a4e
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-09-20 20:21:46 +0300

    firstpush

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
-export function runFetches() {
-    return fetch(`https://pixabay.com/api/?key=29731703-4e8659812dd82e74a79e4fb84&q=cat&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response => response.json())
-        
+export {runFetches}
+function runFetches(query, page) {
+    return fetch(`https://pixabay.com/api/?key=29731703-4e8659812dd82e74a79e4fb84&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+        .then(response => {
+          
+            return response.json()
+        })
+       
 
 }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,85 @@
 import './css/styles.css';
-import cardTmpl from './card.hbs';
+import 'simplelightbox/dist/simple-lightbox.min.css'
+import SimpleLightbox from 'simplelightbox'
+
 import { runFetches } from './api.js';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-    searchButton: document.querySelector('.search-form__button'),
-    searchInput: document.querySelector('.search-form__input'),
+    moreBtn: document.querySelectorAll('.footer-button'),
+    searchForm: document.querySelector('#search-form'),
     galleryArea: document.querySelector('.gallery'),
 };
 
+let page = "";
+let query = "";
+refs.searchForm.addEventListener("submit", onSearch);
 
-
-// function onImageInput(name) {
-//     name = refs.searchInput.value.trim()
-    // if (name === '') {
-    //     return (refs.galleryArea.innerHTML = '')
-    // }
-
-runFetches()
-        .then((data) => {
-            renderGallery(data.hits)
-           //simpleLightBox = new SimpleLightbox('.gallery a').refresh()
-            //alertImagesFound(data)
-
-            
-        })
-        .catch(error => console.log(error))
-// }
-
-
-function renderGallery(image) {
-    const markup = image
-    .map((webformatURL,tags,likes,viewes,comments,downloads ) => {
-        return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:${likes}$</b>
-    </p>
-    <p class="info-item">
-      <b>Views:${viewes}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments:${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads:${downloads}</b>
-    </p>
-  </div>
-</div>`
+function onSearch(e) {
+    e.preventDefault()
+    page = 1;
+    query = e.currentTarget.searchQuery.value.trim()
+  if (query === '') {
+        return Notify.failure("Oh sorry, there are no images matching your search query. Please try again.")
+  }
+    
+    runFetches(query, page)
+    .then((r) => {
+        
+        render(r.hits);
+        Notify.success(`We found ${r.totalHits} images.`); 
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh() 
+         
+             
     })
-    .join('')
-    //refs.searchButton.addEventListener('submit', onImageInput)
+    .catch(error => console.log(error))
 }
+function endOfcollectionCheck(totalHits, page){
+    const totalPages = Math.ceil(totalHits / 40);
+        if (page >= totalPages) {
+            Notify.info("We're sorry, but you've reached the end of search results.");
+
+            return;
+    }
+};
+
+function render(items) {
+  const markup =items
+        .map(item => {
+        const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = item
+        return `
+            <a class="gallery__link" href="${largeImageURL}">
+            <div class="gallery-item" id="${id}">
+                <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" width = "640" height = "400" />
+                <div class="info">
+                <p class="info-item"><b>Likes: </b>${likes}</p>
+                <p class="info-item"><b>Views: </b>${views}</p>
+                <p class="info-item"><b>Comments: </b>${comments}</p>
+                <p class="info-item"><b>Downloads: </b>${downloads}</p>
+                </div>
+            </div>
+            </a>
+        `
+        })
+    .join('')
+refs.galleryArea.insertAdjacentHTML("beforeend", markup)
+}
+
+
+
+window.addEventListener('scroll', () => {
+    const docRect = document.documentElement.getBoundingClientRect();
+    
+    if (docRect.bottom < document.documentElement.clientHeight + 150) {
+        page+=1;
+        runFetches(query, page).then((r) => {
+        endOfcollectionCheck(r.totalHits, page);
+        render(r.hits);
+         simpleLightBox = new SimpleLightbox('.gallery a').refresh()               
+    }).catch(error => console.log(error))
+    }
+})  
+
 
 
 
